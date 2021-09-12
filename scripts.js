9d3d49ae4d9e154f326cc12656b5635a1fff8f72
timthebanana <90521214+timthebanana@users.noreply.github.com>
2021-09-12 11:14:47 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,3 +5,16 @@ function clickhandler(){
     paragraph = document.getElementById("id1");
     paragraph.innerText = paragraph.innerText + "Howdy again! ";
 }
+
+var note = ["AsharpTreble.png",'Atreble.jpg','Btreble.jpg','CsharpTreble.jpg',
+'Ctreble.jpg','DsharpTreble.jpg.png','Dtreble.jpg','Etreble.jpg','FsharpTreble.jpg',
+'Ftreble.jpg','GsharpTreble.jpg.png','Gtreble.jpg','AsharpBass.jpg','Abass.jpg','Bbass.jpg',
+'CsharpBass.png','Cbass.jpg','DsharpBass.png','Dbass.jpg','Ebass.jpg','FsharpBass.png','Fbass.jpg','GsharpBass.png','Gbass.jpg']
+var noteL = ['A#','A','B','C#','C','D#','D','E','F#','F','G#','G','A#','A','B','C#','C','D#','D','E','F#','F','G#','G']
+var option = note[Math.floor(Math.random()*24)]
+//var answer = option == noteL[option]
+//var option = note[0]
+//document.getElementById('note').innerHTML = option;
+document.getElementById('note').src = option;
+
+
