182ddd838674c5ec97f1af2e23282b0bd36d6747
timthebanana <90521214+timthebanana@users.noreply.github.com>
2021-09-12 11:57:14 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,11 +10,21 @@ var note = ["AsharpTreble.png",'Atreble.jpg','Btreble.jpg','CsharpTreble.jpg',
 'Ctreble.jpg','DsharpTreble.jpg.png','Dtreble.jpg','Etreble.jpg','FsharpTreble.jpg',
 'Ftreble.jpg','GsharpTreble.jpg.png','Gtreble.jpg','AsharpBass.jpg','Abass.jpg','Bbass.jpg',
 'CsharpBass.png','Cbass.jpg','DsharpBass.png','Dbass.jpg','Ebass.jpg','FsharpBass.png','Fbass.jpg','GsharpBass.png','Gbass.jpg']
+
+var beat = ["sixteenth_note.png","sixteenth_rest.png","eighth_note.png","eighth_rest.png","quarter_note.png","quarter_rest.png","half_note.png","half_rest.png","whole_note.jpg","whole_rest.png"]
+
 var noteL = ['A#','A','B','C#','C','D#','D','E','F#','F','G#','G','A#','A','B','C#','C','D#','D','E','F#','F','G#','G']
+var beatN = ['1/16','1/16','1/8','1/8','1/4','1/4','1/2','1/2','1/1','1/1']
 var option = note[Math.floor(Math.random()*24)]
-//var answer = option == noteL[option]
-//var option = note[0]
-//document.getElementById('note').innerHTML = option;
-document.getElementById('note').src = option;
+var optionB = beat[Math.floor(Math.random()*10)]
+
+if(document.getElementById('note')) {
+    document.getElementById('note').src = option;
+}
+
+if(document.getElementById('beat')) {
+    // document.getElementById('note').src = option;
+    document.getElementById('beat').src = optionB;
+}
 
 
